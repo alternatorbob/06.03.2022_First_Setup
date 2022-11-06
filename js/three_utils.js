3a5330edfa30a353de8ac06b7d5d5289b2c97562
alternatorbob <cbogdan.nastase@gmail.com>
2022-11-06 19:52:21 +0100

    first commit

---
+++ js/three_utils.js
@@ -0,0 +1,6 @@
+window.addEventListener("resize", onResize, false);
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
